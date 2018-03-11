4ae107626005ff03e0fd17b190baea55d215f170
Denis Ilyushchenko <iluschenko.d.g@hotmail.com>
2018-03-11 20:53:31 +0300

    Добавлены политка поведения SoupFaultPolicy

---
+++ HCS/Polices/ActionByFault.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCS.Polices
+{
+    public class ActionByFault
+    {
+        public string[] ErrorCodes { get; set; }
+        public Actions Action { get; set; }
+    }
+}
